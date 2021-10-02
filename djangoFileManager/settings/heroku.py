2cbb5961eb7288572c56172854b12b29ecd767cb
alinnegrazielle <alinnegraziellecontato@gmail.com>
2021-10-02 20:30:06 -0300

    ready to deploy

---
+++ djangoFileManager/settings/heroku.py
@@ -0,0 +1,15 @@
+import environ
+
+from djangoFileManager.settings.base import *
+
+env = environ.Env()
+
+DEBUG = env.bool("DEBUG", False)
+
+SECRET_KEY = env("SECRET_KEY")
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+DATABASES = {
+    "defalt": env.db(),
+}
--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoFileManager.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "djangoFileManager.settings.base")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
