6924b746f8c20b24dc176a40d98a838c97d0a2f8
alinnegrazielle <alinnegraziellecontato@gmail.com>
2021-10-02 19:03:40 -0300

    lib django-on-heroku

--- djangoFileManager/settings.py
+++ djangoFileManager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
+
+django_on_heroku.settings(locals())
