3930fe6a8654d8b21b9a33097bfe856dc15d395c
alinnegrazielle <alinnegraziellecontato@gmail.com>
2021-10-02 16:35:00 -0300

    connection to postgresql

--- djangoFileManager/settings.py
+++ djangoFileManager/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'djangoFileManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'materiais',
+        'USER': 'postgres',
+        'PASSWORD': 'postgresql',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
