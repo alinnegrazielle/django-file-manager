88a2e4e52591c432692c579fa0a1489d6f36a99b
alinnegrazielle <alinnegraziellecontato@gmail.com>
2021-10-03 15:01:08 -0300

    updating settings

--- djangoFileManager/wsgi.py
+++ djangoFileManager/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "djangoFileManager.settings.base")
+                      "djangoFileManager.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                          "djangoFileManager.settings.base")
+                          "djangoFileManager.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
