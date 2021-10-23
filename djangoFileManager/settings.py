26c9f06c4174a6e1ecfd26ab234d5a608c627261
alinnegrazielle <alinnegraziellecontato@gmail.com>
2021-10-23 17:46:56 -0300

    created gitignore

--- djangoFileManager/settings.py
+++ djangoFileManager/settings.py
@@ -126,9 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = [
+    os.path.join(BASE_DIR, 'media')
 ]
 
 # Default primary key field type
